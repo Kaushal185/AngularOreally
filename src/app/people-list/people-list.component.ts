b582da2e757bd9c7214245e7763670a72fa624bc
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-11-15 11:58:13 +0530

    ngFor with ngIf

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent {
+  showList:boolean = true;
+  people = [
+    {
+      name : 'John',
+      age: 34,
+      hairColor:'brown',
+    },
+    {
+      name : 'michael',
+      age: 14,
+      hairColor:'green',
+    },
+    {
+      name : 'bob',
+      age: 54,
+      hairColor:'black',
+    }
+  ];
+}
