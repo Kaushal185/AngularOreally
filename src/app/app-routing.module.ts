ba7deafad22b247844603fc1fadb012efc226454
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-11-16 16:02:02 +0530

    facing difficulties in routing to counter-button (/counter) in my code:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './counter/counter.component';
 import { ExampleComponent } from './example/example.component';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { CounterPageComponent } from './counter-page/counter-page.component';
+import { CounterComponent } from './counter/counter.component';
 const routes: Routes = [
   {path:'counter-page',component:CounterPageComponent},
   {path:'example',component:ExampleComponent},
   {path:'people-list',component:PeopleListComponent},
+  {path:'',redirectTo:'example',pathMatch:'full'},
+  // {path:'counter',component:CounterComponent},
 ];
 
 @NgModule({
